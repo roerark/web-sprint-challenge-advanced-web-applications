fa159efa75e66014494f92840ef2d8d3a4cf7eb4
joe-park <parkjs3@yahoo.com>
2021-03-26 15:02:51 -0700

    mvp

--- src/components/BubblePage.js
+++ src/components/BubblePage.js
@@ -2,12 +2,22 @@ import React, { useEffect, useState } from "react";
 
 import Bubbles from "./Bubbles";
 import ColorList from "./ColorList";
+import { axiosWithAuth } from "../helpers/axiosWithAuth";
 
 const BubblePage = () => {
   const [colorList, setColorList] = useState([]);
+  useEffect(() => {
+    axiosWithAuth()
+      .get("/api/colors")
+      .then((res) => {
+        setColorList(res.data);
+      })
+      .catch((err) => console.log(err));
+  }, []);
 
   return (
     <div className="container">
+      <h1>Bubble Page</h1>
       <ColorList colors={colorList} updateColors={setColorList} />
       <Bubbles colors={colorList} />
     </div>
